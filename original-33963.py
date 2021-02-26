a70640960887dcddd169f92d8063c40713173189
Trynet123 <su508102@yahoo.co.jp>
2021-02-26 11:02:03 +0900

    Initial commit

---
+++ original-33963.py
@@ -0,0 +1,11 @@
+import pygame
+import sys
+import random
+from pygame.locals import *
+
+# 色の定義
+WHITE = (255, 255, 255)
+BLACK = (  0,   0,   0)
+RED   = (255,   0,   0)
+CYAN  = (  0, 255, 255)
+BLINK = [(224,255,255), (192, 240,255), (128,224,255), (64,192,)]
