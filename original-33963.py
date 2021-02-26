a94adb5628cd42890f271ff2515fd24fabea7223
Trynet123 <su508102@yahoo.co.jp>
2021-02-26 11:25:57 +0900

    画像の読み込み

--- original-33963.py
+++ original-33963.py
@@ -9,3 +9,40 @@ BLACK = (  0,   0,   0)
 RED   = (255,   0,   0)
 CYAN  = (  0, 255, 255)
 BLINK = [(224,255,255), (192, 240,255), (128,224,255), (64,192,255), (128,224,255), (192,240,255)]
+
+# 画像の読み込み
+imgTitle = pygame.image.load("image/title.png")
+imgWall = pygame.image.load("image/wall.png")
+imgWall2 = pygame.image.load("image/wall2.png")
+imgDark = pygame.image.load("image/dark.png")
+imgPara = pygame.image.load("image/parameter.png")
+imgBtlBG = pygame.image.load("image/btlbg.png")
+imgEnemy = pygame.image.load("image/enemy0.png")
+imgItem = [
+    pygame.image.load("image/potion.png"),
+    pygame.image.load("image/blaze_gem.png"),
+    pygame.image.load("image/spoiled.png"),
+    pygame.image.load("image/apple.png"),
+    pygame.image.load("image/meat.png")
+]
+imgFloor = [
+    pygame.image.load("image/floor.png"),
+    pygame.image.load("image/tbox.png"),
+    pygame.image.load("image/cocoon.png"),
+    pygame.image.load("image/stairs.png")
+]
+imgPlayer = [
+    pygame.image.load("image/mychr0.png"),
+    pygame.image.load("image/mychr1.png"),
+    pygame.image.load("image/mychr2.png"),
+    pygame.image.load("image/mychr3.png"),
+    pygame.image.load("image/mychr4.png"),
+    pygame.image.load("image/mychr5.png"),
+    pygame.image.load("image/mychr6.png"),
+    pygame.image.load("image/mychr7.png"),
+    pygame.imgae.load("image/mychr8.png")
+]
+imgEffect = [
+    pygame.image.load("image/effect_a.png"),
+    pygame.image.load("image/effect_b.png")
+]
